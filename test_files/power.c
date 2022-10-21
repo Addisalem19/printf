f6630ce2caf1c579aefff57dc88e599d06e6f7e5
Addisalem <addisalemgirma13@gmail.com>
2022-10-21 15:41:26 -0700

    team task

---
+++ test_files/power.c
@@ -0,0 +1,6 @@
+long long int power(int base, int x){
+
+	int result=1;
+
+	for (x; x>0; x--)
+	{
---
+++ test_files/print_binary.c
@@ -0,0 +1,6 @@
+#include "main.h"
+
+int print_binary(int number){
+	int arr[32], i = 0, count = 0;
+
+	while (number >= 2){
