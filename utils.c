96524dfa765f697c0066b25e9cb4c20353b23ee1
Yetnayet Abebe <101176690+YetuAb@users.noreply.github.com>
2022-10-21 23:27:13 +0300

    utils

--- utils.c
+++ utils.c
@@ -33,7 +33,7 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 
 	buffer[i++] = map_to[ascii_code / 16];
 	buffer[i] = map_to[ascii_code % 16];
-
+	
 	return (3);
 }
 
@@ -53,3 +53,34 @@ int is_digit(char c)
 
 /**
  * convert_size_number - Casts a number to the specified size
+ * @num: Number to be casted
+ * @size: Number indicating the type to be casted
+ *
+ * Return: Casted value of num
+ */
+long int convert_size_number(long int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((short)num);
+	
+	return ((int)num);
+}
+
+/**
+* convert_size_unsgnd - Casts a number to the specified size
+* @num: Number to be casted
+* @size: Number indicating the type to be casted
+*
+* Return: Casted value of num
+*/
+long int convert_size_unsgnd(unsigned long int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((unsigned short)num);
+	
+	return ((unsigned int)num);
+}
