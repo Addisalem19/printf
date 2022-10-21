37b7f1e691dee537e5417b56190c01062b3a908f
Yetnayet Abebe <101176690+YetuAb@users.noreply.github.com>
2022-10-22 02:07:56 +0300

    print_decimal

--- test_files/print_decimal.c
+++ test_files/print_decimal.c
@@ -2,10 +2,12 @@
 
 int print_decimal(int value){
 	int count = 0;
+	
 	if (value < 0) {
 		count += _putchar('-');
 		value = value * -1;
 	}
+	
 	if (value/10)
 	count += print_decimal(value/10);
 	
